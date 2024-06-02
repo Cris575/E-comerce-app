73dcda5e85d92a2a23d7223c6028cabeec482983
Cristian Gonzalez Herrera <cg691539@gmail.com>
2024-06-02 13:38:21 -0600

    commit

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,17 +1,61 @@
 import { View } from "react-native";
 import { TextInput, Button } from "react-native-paper";
 import { globalStyles } from "../../../styles";
+import { useFormik } from "formik";
+import { initialValues, validateOnChange } from "./RegisterForm.form";
 
 export function RegisterForm(props) {
   const { showLogin } = props;
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validateOnChange(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      console.log("form enviado");
+      console.log(formValue);
+    },
+  });
+
   return (
     <View>
-      <TextInput label="Correo electronico" style={globalStyles.form.input} autoCapitalize="none" />
-      <TextInput label="Nombre de usuario" style={globalStyles.form.input} autoCapitalize="none" />
-      <TextInput label="Contraseña" style={globalStyles.form.input} secureTextEntry />
-      <TextInput label="Repetir Contraseña" style={globalStyles.form.input} secureTextEntry />
+      <TextInput
+        label="Correo electronico"
+        style={globalStyles.form.input}
+        autoCapitalize="none"
+        onChangeText={(text) => formik.setFieldValue("email", text)}
+        value={formik.values.email}
+        error={formik.errors.email}
+      />
+      <TextInput
+        label="Nombre de usuario"
+        style={globalStyles.form.input}
+        autoCapitalize="none"
+        onChangeText={(text) => formik.setFieldValue("username", text)}
+        value={formik.values.username}
+        error={formik.errors.username}
+      />
+      <TextInput
+        label="Contraseña"
+        style={globalStyles.form.input}
+        secureTextEntry
+        onChangeText={(text) => formik.setFieldValue("password", text)}
+        value={formik.values.password}
+        error={formik.errors.password}
+      />
+      <TextInput
+        label="Repetir Contraseña"
+        style={globalStyles.form.input}
+        secureTextEntry
+        onChangeText={(text) => formik.setFieldValue("repeatPassword", text)}
+        value={formik.values.repeatPassword}
+        error={formik.errors.repeatPassword}
+      />
 
-      <Button mode="contained" style={globalStyles.form.btnSubmit}>
+      <Button
+        mode="contained"
+        style={globalStyles.form.btnSubmit}
+        onPress={formik.handleSubmit}
+        loading={formik.isSubmitting}>
         Registrase
       </Button>
 
