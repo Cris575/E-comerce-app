2462d12c0ad1d1f7bd7cb5396796a7705924ec6e
Cristian Gonzalez Herrera <cg691539@gmail.com>
2024-06-02 12:56:50 -0600

    commit

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,10 +1,27 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View } from "react-native";
+import { TextInput, Button } from "react-native-paper";
+import { globalStyles } from "../../../styles";
 
-export function RegisterForm() {
+export function RegisterForm(props) {
+  const { showLogin } = props;
   return (
     <View>
-      <Text>RegisterForm</Text>
+      <TextInput label="Correo electronico" style={globalStyles.form.input} autoCapitalize="none" />
+      <TextInput label="Nombre de usuario" style={globalStyles.form.input} autoCapitalize="none" />
+      <TextInput label="Contraseña" style={globalStyles.form.input} secureTextEntry />
+      <TextInput label="Repetir Contraseña" style={globalStyles.form.input} secureTextEntry />
+
+      <Button mode="contained" style={globalStyles.form.btnSubmit}>
+        Registrase
+      </Button>
+
+      <Button
+        mode="text"
+        style={globalStyles.form.btnText}
+        labelStyle={globalStyles.form.btnTextLabel}
+        onPress={showLogin}>
+        Iniciar sesión
+      </Button>
     </View>
   );
 }
--- src/screens/Auth/AuthScreen/AuthScreen.js
+++ src/screens/Auth/AuthScreen/AuthScreen.js
@@ -5,14 +5,15 @@ import { LoginForm, RegisterForm } from "../../../components/Auth";
 import logo from "../../../../assets/Logo.png";
 
 export function AuthScreen() {
-  const [showLogin, setShowLogin] = useState(true);
+  const [showLogin, setShowLogin] = useState(false);
 
+  const onShowLoginregistrer = () => setShowLogin((prevState) => !prevState);
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
-      <KeyboardAvoidingView behavior={(Platform.OS = "ios" ? "padding" : "height")}>
-        {showLogin ? <LoginForm /> : <RegisterForm />}
-      </KeyboardAvoidingView>
+      {/* <KeyboardAvoidingView behavior={(Platform.OS = "and" ? "padding" : "height")}> */}
+      {showLogin ? <LoginForm /> : <RegisterForm showLogin={onShowLoginregistrer} />}
+      {/* </KeyboardAvoidingView> */}
     </View>
   );
 }
