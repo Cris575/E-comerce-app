40dbba348f1886c7983abc530f385137e290dbd8
Cristian Gonzalez Herrera <cg691539@gmail.com>
2024-06-02 19:03:16 -0600

    commit

--- src/context/AuthConetxt.js
+++ src/context/AuthConetxt.js
@@ -1,14 +1,30 @@
 import { useState, useEffect, createContext } from "react";
-import { date } from "yup";
+import { storageCtrl } from "../api";
 
 export const AuthContext = createContext();
 
 export function AuthProvider(props) {
   const { children } = props;
+  useEffect(() => {
+    recoverySession();
+  }, []);
+
+  const recoverySession = async () => {
+    const token = await storageCtrl.getToken();
+    console.log("TOKEN -->", token);
+  };
+
+  const login = async (token) => {
+    try {
+      await storageCtrl.setToken(token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const data = {
     user: null,
-    login: () => console.log("LOGIN"),
+    login,
     logout: () => console.log("LOGOUT"),
     updateUser: () => console.log("UPDATE_USER"),
   };
