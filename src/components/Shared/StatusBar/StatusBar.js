1f06e2804f5ec2d9e1402ddf01c306acc9bf9ba1
Cristian Gonzalez Herrera <cg691539@gmail.com>
2024-06-04 08:39:45 -0600

    commit

---
+++ src/components/Shared/StatusBar/StatusBar.js
@@ -0,0 +1,13 @@
+import { StatusBar as StatusBarRN, SafeAreaView } from "react-native";
+import { styled } from "./StatusBar.styles";
+
+export function StatusBar(props) {
+  const { backgroundColor, ...rest } = props;
+  const styles = styled(backgroundColor);
+  return (
+    <>
+      <StatusBarRN backgroundColor={backgroundColor} {...rest} />
+      <SafeAreaView style={styles.safeAreaView} />
+    </>
+  );
+}
