86caa15e8f9fcd0bda506663a82d5ccf03609a14
Cristian Gonzalez Herrera <cg691539@gmail.com>
2024-06-05 20:25:07 -0600

    commit

---
+++ src/components/Account/UserInfo/UserInfo.styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+import { bool } from "yup";
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 100,
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
--- src/screens/Account/AccountScreen.js
+++ src/screens/Account/AccountScreen.js
@@ -1,10 +1,10 @@
-import { Text } from "react-native";
 import { Layout } from "../../layouts";
+import { UserInfo } from "../../components/Account";
 
 export function AccountScreen() {
   return (
     <Layout.Basic>
-      <Text>AccountScreen</Text>
+      <UserInfo />
     </Layout.Basic>
   );
 }
