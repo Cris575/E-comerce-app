661945aa835f4b53dbbc312c2f816a3bad10cab4
Cristian Gonzalez Herrera <cg691539@gmail.com>
2024-06-05 22:42:06 -0600

    commit

--- src/screens/Account/ChangePasswordScreen.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export function ChangePasswordScreen() {
-  return (
-    <View>
-      <Text>AccountScreen</Text>
-    </View>
-  );
-}
--- src/screens/Account/ChangeUsernameScreen.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export function ChangeUsernameScreen() {
-  return (
-    <View>
-      <Text>AccountScreen</Text>
-    </View>
-  );
-}
