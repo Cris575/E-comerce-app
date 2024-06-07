bea85b62f84474b066a9cf9f89d20dc3eb7bf4b6
Cristian Gonzalez Herrera <cg691539@gmail.com>
2024-06-07 07:13:18 -0600

    commit

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,9 +1,27 @@
 import { SafeAreaView, Text } from "react-native";
 import { useAuth } from "../../hooks";
 import { Button } from "react-native-paper";
+import { useState, useEffect } from "react";
+import { homeBannerCtrl } from "../../api";
+import Toast from "react-native-root-toast";
 
 export function HomeScreen() {
   const { logout } = useAuth();
+  const [banner, setBanner] = useState(null);
+
+  useEffect(() => {
+    getBanners();
+  }, []);
+
+  const getBanners = async () => {
+    try {
+      const response = await homeBannerCtrl.getAll();
+      setBanner(response?.data || null);
+    } catch (error) {
+      Toast.show("Error al obtener los banners", { positio: Toast.positions.CENTER });
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>HomeScreen</Text>
