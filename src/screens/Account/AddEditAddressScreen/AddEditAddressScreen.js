235afa7f0019e18d577386aa5bf63a7baa8671ca
Cristian Gonzalez Herrera <cg691539@gmail.com>
2024-06-06 20:45:00 -0600

    commit

--- src/screens/Account/AddEditAddressScreen/AddEditAddressScreen.js
+++ src/screens/Account/AddEditAddressScreen/AddEditAddressScreen.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { View, Text } from "react-native";
 import { TextInput, Button } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
-// import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { useFormik } from "formik";
 import Toast from "react-native-root-toast";
 // import { addressCtrl } from "../../../api";
@@ -12,20 +12,27 @@ import { globalStyles } from "../../../styles";
 import { styles } from "./AddEditAddressScreen.styles";
 
 export function AddEditAddressScreen() {
-  return (
-    <View style={styles.container}>
-      <TextInput label="Titulo" style={globalStyles.form.input} />
-      <TextInput label="Nombre y apellidos" style={globalStyles.form.input} />
-      <TextInput label="Direccion" style={globalStyles.form.input} />
-      <TextInput label="Codigo postal" style={globalStyles.form.input} />
-      <TextInput label="Polacion" style={globalStyles.form.input} />
-      <TextInput label="Estado" style={globalStyles.form.input} />
-      <TextInput label="Pais" style={globalStyles.form.input} />
-      <TextInput label="Telefono" style={globalStyles.form.input} />
+  const navigation = useNavigation();
+  useEffect(() => {
+    navigation.setOptions({ title: "Crear direccion" });
+  }, []);
 
-      <Button mode="contained" style={[globalStyles.form.btnSubmit, styles.btnSubmit]}>
-        "Crear direccion"
-      </Button>
-    </View>
+  return (
+    <KeyboardAwareScrollView extraScrollHeight={35}>
+      <View style={styles.container}>
+        <TextInput label="Titulo" style={globalStyles.form.input} />
+        <TextInput label="Nombre y apellidos" style={globalStyles.form.input} />
+        <TextInput label="Direccion" style={globalStyles.form.input} />
+        <TextInput label="Codigo postal" style={globalStyles.form.input} />
+        <TextInput label="Polacion" style={globalStyles.form.input} />
+        <TextInput label="Estado" style={globalStyles.form.input} />
+        <TextInput label="Pais" style={globalStyles.form.input} />
+        <TextInput label="Telefono" style={globalStyles.form.input} />
+
+        <Button mode="contained" style={[globalStyles.form.btnSubmit, styles.btnSubmit]}>
+          Crear direccion
+        </Button>
+      </View>
+    </KeyboardAwareScrollView>
   );
 }
