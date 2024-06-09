b105db37fd7d293b6ce5349a0bf2bb039659e90f
Cristian Gonzalez Herrera <cg691539@gmail.com>
2024-06-09 11:39:41 -0600

    commit

--- src/api/cart.js
+++ src/api/cart.js
@@ -2,14 +2,32 @@ import { map, forEach } from "lodash";
 import { ENV } from "../utils";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-async function addCart(productId) {
-  const products = [];
+async function getAllProducts() {
+  const response = await AsyncStorage.getItem(ENV.STORAGE.CART);
 
-  products.push({ id: productId, quatity: 1 });
+  if (!response) {
+    return [];
+  } else {
+    return JSON.parse(response);
+  }
+}
+
+async function addCart(productId) {
+  const products = await getAllProducts();
+  const objIndex = products.findIndex((product) => product.id === productId);
+  if (objIndex < 0) {
+    products.push({ id: productId, quatity: 1 });
+  } else {
+    const product = products[objIndex];
+    products[objIndex].quatity = product.quatity + 1;
+  }
+
+  console.log(products);
 
   await AsyncStorage.setItem(ENV.STORAGE.CART, JSON.stringify(products));
 }
 
 export const cartCtrl = {
-  addCart,
+  add: addCart,
+  getAll: getAllProducts,
 };
